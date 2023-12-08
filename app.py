e6d7ba3f2f7701acc974752abbe339863238270d
anarugna1~ <azrugna@yahoo.com>
2023-12-08 11:31:30 -0300

    seguimos bd

--- app.py
+++ app.py
@@ -8,86 +8,86 @@ mysql = MySQL()
 app.config['MYSQL_DATABASE_HOST']='localhost'
 app.config['MYSQL_DATABASE_USER']='root'
 app.config['MYSQL_DATABASE_PASSWORD']=''
-app.config['MYSQL_DATABASE_BD']='sistema'
+app.config['MYSQL_DATABASE_BD']='usuario'
 mysql.init_app(app)
 
 @app.route("/")
-def index():
-    sql = "SELECT * FROM `sistema`.`usuario`;"
+def usuario():
+    sql = "SELECT * FROM `usuario`.`inscribite`;"
     conn=mysql.connect()
     cursor=conn.cursor()
     cursor.execute(sql)
-    empleados=cursor.fetchall()
-    # print(empleados)
+    inscribite=cursor.fetchall()
+    # print(inscribite)
     conn.commit()
-    return render_template("usuario.html",empleados=empleados)
+    return render_template("usuario.html",inscribite=inscribite)
 
 @app.route("/create")
 def create():
-    return render_template("index.html")
+    return render_template("usuario.html")
 
 
-@app.route("/store",methods=["POST"])
-def storage():
-    _nombre=request.form['txtNombre']
-    _correo=request.form['txtCorreo']
-    _foto=request.files['txtFoto']
-    now= datetime.now()
-    tiempo= now.strftime("%Y%H%M%S")
-    if _foto.filename!='':
-        nuevoNombreFoto=tiempo+_foto.filename
-        _foto.save("uploads/"+nuevoNombreFoto)
+# @app.route("/store",methods=["POST"])
+# def storage():
+#     _nombre=request.form['txtnombre']
+#     _correo=request.form['txtemail']
+#     _foto=request.files['txtFoto']
+#     now= datetime.now()
+#     tiempo= now.strftime("%Y%H%M%S")
+#     if _foto.filename!='':
+#         nuevoNombreFoto=tiempo+_foto.filename
+#         _foto.save("uploads/"+nuevoNombreFoto)
 
-    sql = "INSERT INTO `sistema`.`empleados` (`id`, `nombre`, `correo`, `foto`) VALUES (NULL, %s, %s, %s);"
-    datos=(_nombre,_correo,nuevoNombreFoto)
+#     sql = "INSERT INTO `usuario`.`inscribite` (`id`, `Nombre y Apellido`, `Correo Electrónico`, `Comprobante de Pago`) VALUES (NULL, %s, %s, %s);"
+#     datos=(_nombre,_correo,nuevoNombreFoto)
 
-    conn=mysql.connect()
-    cursor=conn.cursor()
-    cursor.execute(sql,datos)
-    conn.commit()
-    return render_template("index.html")
+#     conn=mysql.connect()
+#     cursor=conn.cursor()
+#     cursor.execute(sql,datos)
+#     conn.commit()
+#     return render_template("index.html")
     
 
-@app.route("/destroy/<int:id>")
-def destroy(id):
-    conn=mysql.connect()
-    cursor=conn.cursor()
-    sql="DELETE FROM `sistema`.`empleados` WHERE id=%s"
-    cursor.execute(sql,id)
-    conn.commit()
-    return redirect("/")
+# @app.route("/destroy/<int:id>")
+# def destroy(id):
+#     conn=mysql.connect()
+#     cursor=conn.cursor()
+#     sql="DELETE FROM `usuario`.`inscribite` WHERE id=%s"
+#     cursor.execute(sql,id)
+#     conn.commit()
+#     return redirect("/")
 
 
-@app.route("/edit/<int:id>")
-def edit(id):
-    sql = "SELECT * FROM `sistema`.`empleados` WHERE id=%s;"
-    conn=mysql.connect()
-    cursor=conn.cursor()
-    cursor.execute(sql,id)
-    empleados=cursor.fetchall()
-    conn.commit()
+# @app.route("/edit/<int:id>")
+# def edit(id):
+#     sql = "SELECT * FROM `usuario`.`inscribite` WHERE id=%s;"
+#     conn=mysql.connect()
+#     cursor=conn.cursor()
+#     cursor.execute(sql,id)
+#     inscribite=cursor.fetchall()
+#     conn.commit()
     
-    # print(empleados)
-    return render_template("edit.html",empleados=empleados)
+#     # print(inscribite)
+#     return render_template("edit.html",inscribite=inscribite)
 
 
 
 
 
-@app.route('/update', methods=['POST'])
-def update():
-    _nombre=request.form['txtNombre']
-    _correo=request.form['txtCorreo']
-    # _foto=request.files['txtFoto']
-    id=request.form['txtID']
+# @app.route('/update', methods=['POST'])
+# def update():
+#     _nombre=request.form['txtNombre']
+#     _correo=request.form['txtCorreo']
+#     # _foto=request.files['txtFoto']
+#     id=request.form['txtID']
 
-    datos=(_nombre,_correo,id)
-    conn = mysql.connect()
-    cursor = conn.cursor()
-    sql = "UPDATE `sistema`.`empleados` SET `nombre`=%s, `correo`=%s WHERE id=%s;"
-    cursor.execute(sql,datos)
-    conn.commit()
-    return redirect('/')
+#     datos=(_nombre,_correo,id)
+#     conn = mysql.connect()
+#     cursor = conn.cursor()
+#     sql = "UPDATE `usuario`.`inscribite` SET `Nombre y Apellido`=%s, `Correo Electrónico`=%s WHERE id=%s;"
+#     cursor.execute(sql,datos)
+#     conn.commit()
+#     return redirect('/')
 
 
 
